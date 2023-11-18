88cd0980d3f415680be2f83ad1a1f2c0fe860d69
Hamza Elhamdadi <irishlad98@Hamzas-MacBook-Pro.local>
2023-11-18 14:16:34 -0500

    updated TiledQ; updated SARSA

--- RLAlgorithms.py
+++ RLAlgorithms.py
@@ -1,45 +1,66 @@
 import numpy as np
 
-def tiling(range, bins, offset):
-    return np.linspace(range[0], range[1], bins+1)[1:-1] + offset
+class TiledQ():
+    def __init__(self, A, ranges, num_tilings, bins, offsets):
+        self.tilings = []
+        for i in range(num_tilings):
+            t = []
+            for j in range(len(ranges)):
+                t.append(np.linspace(ranges[j,0], ranges[j,1], bins[i,j]+1)[1:-1] + offsets[i,j])
+            self.tilings.append(t)
 
-def tilings(ranges, num, bins, offsets):
-    ts = []
-    for i in range(num):
-        t = []
-        for j in range(len(ranges)):
-            t.append(tiling(ranges[j],bins[i,j],offsets[i,j]))
-        ts.append(t)
-    return np.array(ts)
+        self.tilings     = np.array(self.tilings)
+        self.ranges      = ranges
+        self.A           = A
+        self.state_sizes = [tuple(len(splits)+1 for splits in tiling) for tiling in self.tilings]
+        self.q_tables    = [np.zeros((state_size+(len(self.A),))) for state_size in self.state_sizes]
+        pass
 
-def tile_coding(feat, ts):
-    n = len(feat)
-    codings = []
-    for t in ts:
-        coding = []
-        for i in range(n):
-            coding.append(np.digitize(feat[i],t[i]))
-        codings.append(coding)
-    return np.array(coding)
+    def tile_coding(self, feature):
+        codings = []
+        for t in self.tilings:
+            coding = []
+            for i in range(len(feature)):
+                coding.append(np.digitize(feature[i], t[i]))
+            codings.append(coding)
+        return np.array(codings)
+
+    def value(self, s):
+        codings = self.tile_coding(s)
+        qs = []
+        for a_idx in len(self.A):
+            q = 0
+            for coding, q_table in zip(codings, self.q_tables):
+                q += q_table[tuple(coding)+(a_idx,)]
+            qs.append(q / len(self.tilings))
+        return np.array(qs)
+
+
 
 class ESGNStepSARSA():
-    def __init__(self, ECMDP, epsilon, D, n):
-        self.MDP = ECMDP
+    def __init__(self, MDP, bins, offsets, D, n, epsilon):
+        if not (len(bins) == len(offsets) == D):
+            raise ValueError('bins and offsets must both have length D')
+
+        self.MDP = MDP
+        self.A = self.MDP.A
+        self.q = TiledQ(self.MDP.A, self.MDP.get_feature_ranges(), D, bins, offsets)
         self.alpha = 0.5
         self.epsilon = epsilon
         self.w = np.zeros(D)
         self.n = n
-    
-    def qhat(self, s, a, w):
 
-        pass
+    def next_action(self, s):
+        A_card = len(self.A)
+        probs = np.zeros(A_card) + self.epsilon / A_card
 
-    def next_action(self, s, ):
-            
-        pass
+        q = self.w.dot(self.q.value(s))
+        probs[np.argmax(q)] += (1 - self.epsilon)
+
+        return np.random.choice(self.A, 1, p=probs) 
 
     def run(self):
         while True:
             s0 = self.MDP.s
-            
+            a0 = self.next_action(s0)
         pass
