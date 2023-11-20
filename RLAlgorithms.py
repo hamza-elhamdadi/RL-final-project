67547a9b2936c9278f70164508375dbb0c17a535
Hamza Elhamdadi <irishlad98@Hamzas-MacBook-Pro.local>
2023-11-20 11:55:20 -0500

    complete ESGNStepSARSA function (not tested)

--- RLAlgorithms.py
+++ RLAlgorithms.py
@@ -25,20 +25,26 @@ class TiledQ():
             codings.append(coding)
         return np.array(codings)
 
-    def value(self, s):
+    def value(self, s, a=None):
         codings = self.tile_coding(s)
+        
         qs = []
         for a_idx in len(self.A):
             q = 0
             for coding, q_table in zip(codings, self.q_tables):
                 q += q_table[tuple(coding)+(a_idx,)]
             qs.append(q / len(self.tilings))
+        
+        if a:
+            a_idx = self.A.index(a)
+            return qs[a_idx]
+        
         return np.array(qs)
 
 
 
 class ESGNStepSARSA():
-    def __init__(self, MDP, bins, offsets, D, n, epsilon):
+    def __init__(self, MDP, bins, offsets, D, n, epsilon, gamma, alpha, num_episodes=500):
         if not (len(bins) == len(offsets) == D):
             raise ValueError('bins and offsets must both have length D')
 
@@ -46,21 +52,64 @@ class ESGNStepSARSA():
         self.A = self.MDP.A
         self.q = TiledQ(self.MDP.A, self.MDP.get_feature_ranges(), D, bins, offsets)
         self.alpha = 0.5
-        self.epsilon = epsilon
         self.w = np.zeros(D)
         self.n = n
+        self.epsilon = epsilon
+        self.gamma = gamma
+        self.alpha = alpha
+
+        self.num_episodes = num_episodes
+
+    def grad_qhat(self, s, a):
+        return self.q.value(s, a)
+
+    def qhat(self, s, a=None):
+        return self.w.dot(self.q.value(s, a))
 
     def next_action(self, s):
         A_card = len(self.A)
         probs = np.zeros(A_card) + self.epsilon / A_card
 
-        q = self.w.dot(self.q.value(s))
+        q = self.qhat(s)
         probs[np.argmax(q)] += (1 - self.epsilon)
 
         return np.random.choice(self.A, 1, p=probs) 
 
     def run(self):
-        while True:
-            s0 = self.MDP.s
-            a0 = self.next_action(s0)
-        pass
+        for _ in range(self.num_episodes):
+            self.MDP.reset()
+
+            states = []
+            actions = []
+            rewards = []
+
+            states.append(self.MDP.s)
+
+            a = self.next_action(self.MDP.s)
+            actions.append(a)
+
+            T, t = float('inf'), 0
+            while True:
+                if t < T:
+                    self.MDP.next_state(a)
+                    rewards.append(self.MDP.reward(self.MDP.s, a))
+                    states.append(self.MDP.s)
+
+                    if self.MDP.is_terminal():
+                        T = t + 1
+                    else:
+                        a = self.next_action(self.MDP.s)
+                        actions.append(a)
+                tau = t - self.n + 1
+                if tau >= 0:
+                    lower = tau + 1
+                    upper = min(tau+self.n, T)
+                    G = 0
+                    for i in range(lower, upper+1):
+                        G += self.gamma**(i-tau-1) * rewards[i]
+                        if t + self.n < T:
+                            G += self.gamma**self.n * self.qhat(states[tau+self.n], actions[tau+self.n])
+                        self.w += self.alpha*(G - self.qhat(states[tau], actions[tau])) * self.grad_qhat(states[tau], actions[tau])
+                
+                if tau == T+1:
+                    break
