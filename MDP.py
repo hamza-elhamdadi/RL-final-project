3a4283df09c70dc14c125ad42d151acd34aa6ef2
akshatchhabra <akshatchhabr@umass.edu>
2023-11-26 23:37:11 -0500

    feat: add pendulum env implementation

--- MDP.py
+++ MDP.py
@@ -14,9 +14,9 @@ class EpisodicContinuousMDP(ABC):
     def next_state(self, a):
         pass
 
-    @abstractmethod
-    def is_terminal(self):
-        pass
+    # @abstractmethod
+    # def is_terminal(self):
+    #     pass
 
     @abstractmethod
     def reward(self, s=None, a=None):
@@ -26,9 +26,9 @@ class EpisodicContinuousMDP(ABC):
     def run_episode(self, policy):
         pass
 
-    @abstractmethod
-    def get_feature_ranges(self):
-        pass
+    # @abstractmethod
+    # def get_feature_ranges(self):
+    #     pass
 
 class MountainCar(EpisodicContinuousMDP):
     def __init__(self, A=[-1,0,1], x_bounds=[-1.2,0.6], v_bounds=[-0.07, 0.07]):
@@ -78,12 +78,67 @@ class MountainCar(EpisodicContinuousMDP):
         return np.array([[self.x_lower,self.x_upper],[self.v_lower,self.v_upper]])
 
 
-# class InvertedPendulum(EpisodicContinuousMDP):
-#     def __init__():
-#         pass
+class InvertedPendulum(EpisodicContinuousMDP):
+    def __init__(self):
+        self.s = self.initial_state()
+        self.m = 1.0
+        self.l = 1.0
+        self.g = 10.0
+        self.delta_t = 0.05
+        self.theta_upper = np.pi
+        self.theta_dot_upper = 8.0
+        self.tau_upper = 2.0
+
+    def initial_state(self):
+        # random angle in [-pi, pi], random angular velocity in [-1,1].
+        return [np.random.uniform(-np.pi, np.pi), np.random.uniform(-1, 1)]
+
+    def reset(self):
+        self.s = self.initial_state()
+    
+    def normalize_theta(self, theta):
+        return ((theta + np.pi) % (2 * np.pi)) - np.pi
+    
+    def next_state(self, tau):
+        # action is the torque, tau
+        theta, theta_dot = self.s
+        if tau > self.tau_upper:
+            tau = self.tau_upper
+        elif tau < -self.tau_upper:
+            tau = -self.tau_upper
+        new_theta_dot =  theta_dot + (3 * self.g / (2 * self.l) * np.sin(theta) + 3.0 / (self.m * self.l**2) * tau) * self.delta_t
+
+        if new_theta_dot > self.theta_dot_upper:
+            new_theta_dot = self.theta_dot_upper
+        elif new_theta_dot < -self.theta_dot_upper:
+            new_theta_dot = -self.theta_dot_upper
+
+        new_theta = theta + new_theta_dot*self.delta_t
+
+        self.s = [new_theta, new_theta_dot]
+
+    def reward(self, tau):
+        theta, theta_dot = self.s
+        theta = self.normalize_theta(theta)
+        return -(theta ** 2 + 0.1 * (theta_dot ** 2) + 0.001 * (tau ** 2))
+        
+
+    def run_episode(self, policy):
+        G = 0
+        for _ in range(200):
+            action = policy(self.s)
+            # added reward first because reward function does not use next state to determine reward
+            print(self.reward(action))
+            G += self.reward(action)
+            self.next_state(action)
+        self.reset()
+        return G
 
 
 if __name__ == '__main__':
-    mc = MountainCar()
-    print(mc.run_episode(lambda s: -1 if s[1] < 0 else 1))
+    # mc = MountainCar()
+    # print(mc.run_episode(lambda s: -1 if s[1] < 0 else 1))
+
+    pendulum = InvertedPendulum()
+    print(pendulum.run_episode(lambda s : 1))
     
