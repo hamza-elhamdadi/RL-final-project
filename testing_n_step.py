51222eb0e8dc4ed0ecf14983263f78b393f38e4c
Hamza Elhamdadi <irishlad98@Hamzas-MacBook-Pro.local>
2023-12-08 21:19:49 -0500

    some testing

--- testing_n_step.py
+++ testing_n_step.py
@@ -1,6 +1,6 @@
 from MDP import MountainCar, CartPole
 from RLAlgorithms import ESGNStepSARSA, TrueOnlineSARSALambda
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 cp = CartPole()
@@ -8,10 +8,11 @@ cp = CartPole()
 # alg_q = TrueOnlineSARSALambda(cp, M=10, tdr=0.6)
 # alg_q.run()
 
-alg = ESGNStepSARSA(cp, M=10, n=5, alpha=0.001, approach='epsilon-greedy')
+alg = ESGNStepSARSA(cp, M=10, n=5, alpha=0.01, approach='epsilon-greedy')
 # alg.w = alg_q.w
 # alg.epsilon = alg_q.epsilon
 Gs = np.array(alg.run())
 
 plt.clf()
 plt.plot(range(len(Gs)), Gs)
+plt.savefig('temp.png', facecolor='white')
